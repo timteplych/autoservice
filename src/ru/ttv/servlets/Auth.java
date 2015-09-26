cc04fbb1dc4675afb618a560140a73397dff10f1
tim <timteplych@mail.ru>
2015-09-26 21:24:07 +0800

    update1

---
+++ src/ru/ttv/servlets/Auth.java
@@ -0,0 +1,18 @@
+package ru.ttv.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Created by tim on 26.09.15.
+ */
+@WebServlet("/auth.do")
+public class Auth extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
